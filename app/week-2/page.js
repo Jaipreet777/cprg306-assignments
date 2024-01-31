f842c678d4222a6376d25e7a98b2bfb1e6b85310
Jaipreet777 <131726190+Jaipreet777@users.noreply.github.com>
2024-01-30 23:03:54 -0700

    Completed week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "./student-info"    
+
+export default function Page() {
+    return (
+        <main>
+            <h1>shopping list   
+            </h1>
+            <StudentInfo/>
+        </main>
+    )
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+
+
+export default function StudentInfo () {
+    return (
+        <div>
+            <p>Jaipreet Singh Dhillon</p>
+            <p>https://github.com/Jaipreet777</p>
+        </div>
+    )
+}
